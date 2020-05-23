5c6b2fd35f140894bc942bb5e766458c7276b9ee
S-Delowar <sayed.buet97@gmail.com>
2020-05-23 15:45:12 +0600

    resume added

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -13,37 +13,40 @@ const Contact = () => {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
-              }
-        })
-        .then(response => response.json())
-        .then(response => {
-            if(response.status === 'success') {
-                alert('Message sent')
-            } else {
-                alert('Message failed to send')
             }
         })
+            .then(response => response.json())
+            .then(response => {
+                if (response.status === 'success') {
+                    alert('Message sent')
+                } else {
+                    alert('Message failed to send')
+                }
+            })
     };
 
     return (
-        <div className="container contact-page ">
-            <div className="d-flex justify-content-center">
-            <h3>Contact with me:</h3>
+        <div className="container contact-page">
+        <div className=" row row align-items-center ">
+            <div className="col-md-6">
+                <h3>Contact with me:</h3>
+                <form className="contactForm" onSubmit={handleSubmit(onSubmit)}>
+                    <input name="name" ref={register({ required: true })} placeholder="Your Name" />
+                    {errors.name && <small className="error">Name is required</small>}
+
+                    <input name="email" ref={register({ required: true })} placeholder="Your Email" />
+                    {errors.email && <small className="error">Email is required</small>}
+
+                    <textarea name="message" ref={register({ required: true })} placeholder="Type your message" />
+                    {errors.message && <small className="error">Message is required</small>}
+
+                    <input type="submit" className="btn btn-primary" />
+                </form>
             </div>
-            <div className="d-flex justify-content-center">
-            <form className="contactForm" onSubmit={handleSubmit(onSubmit)}>
-                <input name="name" ref={register({ required: true })} placeholder="Your Name" />
-                {errors.name && <small className="error">Name is required</small>}
-
-                <input name="email" ref={register({ required: true })} placeholder="Your Email" />
-                {errors.email && <small className="error">Email is required</small>}
-
-                <textarea name="message" ref={register({ required: true })} placeholder="Type your message" />
-                {errors.message && <small className="error">Message is required</small>}
-
-                <input type="submit" className="btn btn-primary" />
-            </form>
+            <div className="col-md-6">
+            hello
             </div>
+        </div>
         </div>
     );
 };
--- src/Component/Resume/Resume.js
+++ src/Component/Resume/Resume.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Resume.css';
 
+
 const Resume = () => {
-    return (
+       return (
         <div className="container resume-page">
-            Resume page
+            
+                 <a href="https://drive.google.com/file/d/15idLjv05kyUS32TgLLW06X_zGqJRmDZA/view?usp=sharing" target="_blank">
+                     <button className="btn btn-danger">Download Resume</button>
+                </a>
         </div>
     );
 };
